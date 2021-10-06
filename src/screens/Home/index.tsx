a93ed10fc71f558805548c06274a3735b0213677
lucasviamaquinas <79279663+lucasviamaquinas@users.noreply.github.com>
2021-10-06 17:27:17 -0300

    Adding typescript and homescreen into project

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+   Container,
+   Title
+} from './styles';
+
+function Home() {
+   return(
+      <Container>
+         <Title>Home</Title>
+      </Container>
+   );
+}
